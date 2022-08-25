1845b7a5bcdff475fd50bdfcb59ed6556d0d9cda
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2022-08-25 10:43:11 -0400

    Update server.js
    chanced port to accept minimist arguement

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Require http module
 const http = require('http');
 // Require fs module
-const fs = require('fs')
+const fs = require('fs');
+const minimist = require('minimist');
 // Require minimist module (make sure you install this one via npm).
 const args = require('minimist')(process.argv.slice(2));
 
@@ -10,7 +11,7 @@ args.name; // joe
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = process.env.PORT || 3000
+const port = args.name || 3000
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
@@ -53,3 +54,4 @@ server.listen(port, () => {
 
 
 // That's it! You're all done!
+ 
