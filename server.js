a892ea07eb80f488c92a8c9dff734cf14fcd698c
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2022-08-25 11:00:36 -0400

    Update server.js
    saved data to a variable

--- server.js
+++ server.js
@@ -16,13 +16,15 @@ const port = args.name || 3000
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-try {
-  const data = fs.readFileSync('./public/index.html', 'utf8');
-  console.log(data);
-} catch (err) {
-  console.error(err);
-}
-
+var data_;
+fs.readFile('./public/index.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    data_ = data;
+    console.log(data);
+  });
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
@@ -39,7 +41,8 @@ try {
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
-    res.end(data)
+    res.write(data_)
+    res.end()
   });
 
 
